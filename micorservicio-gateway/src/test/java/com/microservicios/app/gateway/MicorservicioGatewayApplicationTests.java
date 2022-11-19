004576511679b9f944d919c5bfdfb05c57721334
fran89rbb <fran89rbb@gmail.com>
2022-11-19 18:07:16 +0100

    Creando y configurando gateway

---
+++ micorservicio-gateway/src/test/java/com/microservicios/app/gateway/MicorservicioGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicios.app.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicorservicioGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
