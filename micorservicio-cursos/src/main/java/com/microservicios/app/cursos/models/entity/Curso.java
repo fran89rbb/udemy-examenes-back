eb453abe097f5163d093ca7f75c26d2540c44bf9
fran89rbb <fran89rbb@gmail.com>
2022-12-08 23:12:03 +0100

    Modificando relación en entity Curso con alumnos

--- micorservicio-cursos/src/main/java/com/microservicios/app/cursos/models/entity/Curso.java
+++ micorservicio-cursos/src/main/java/com/microservicios/app/cursos/models/entity/Curso.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -16,8 +17,10 @@ import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotEmpty;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.microservicios.app.alumnos.models.entity.Alumno;
 import com.microservicios.app.examenes.models.entity.Examen;
 
@@ -35,8 +38,14 @@ public class Curso {
 	@Column(name = "create_at")
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date createAt;
-
-	@OneToMany(fetch = FetchType.LAZY)
+	
+	@JsonIgnoreProperties(value = { "curso" }, allowSetters = true)
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "curso", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<CursoAlumno> cursoAlumnos;
+	
+	//-----COMETAMOS PORQUE LA TABLA ALUMNOS YA NO ESTAS EN MARIADB, SINO EN POSTGRES-----
+	//@OneToMany(fetch = FetchType.LAZY)
+	@Transient
 	private List<Alumno> alumnos;
 
 	@ManyToMany(fetch = FetchType.LAZY)
@@ -50,6 +59,7 @@ public class Curso {
 	public Curso() {
 		this.alumnos = new ArrayList<>();
 		this.examenes = new ArrayList<>();
+		this.cursoAlumnos = new ArrayList<>();
 	}
 
 	public Long getId() {
@@ -106,6 +116,22 @@ public class Curso {
 	
 	public void removeExamen(Examen examen) {
 		this.examenes.remove(examen);
+	}
+
+	public List<CursoAlumno> getCursoAlumnos() {
+		return cursoAlumnos;
+	}
+
+	public void setCursoAlumnos(List<CursoAlumno> cursoAlumnos) {
+		this.cursoAlumnos = cursoAlumnos;
+	}
+	
+	public void addCursoAlumnos(CursoAlumno cursoAlumno) {
+		this.cursoAlumnos.add(cursoAlumno);
+	}
+
+	public void removeCursoAlumnos(CursoAlumno cursoAlumno) {
+		this.cursoAlumnos.remove(cursoAlumno);
 	}
 
 }
